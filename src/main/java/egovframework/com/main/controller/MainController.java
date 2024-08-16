8992a4c145444a9c4d69efd1452be151d069ff65
USER <USER@1.209.180.136>
2024-08-16 10:53:58 +0900

    <2024.08.16>/<2024.08.14> 커밋 Cookie/tilse

--- src/main/java/egovframework/com/main/controller/MainController.java
+++ src/main/java/egovframework/com/main/controller/MainController.java
@@ -1,11 +1,21 @@
 package egovframework.com.main.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -70,6 +80,59 @@ public class MainController {
 		List<HashMap<String, Object>> scorelist = mainService.selectStudentScore();
 		System.out.println(2);
 		mv.addObject("list", scorelist);
+		return mv;
+
+	}
+
+	@RequestMapping("/sessionTest.do")
+	public ModelAndView sessionTest(HttpServletRequest request) {
+		ModelAndView mv = new ModelAndView("main");
+
+		HttpSession session =request.getSession(false);
+		if(session == null) {
+			System.out.println("최초방문");
+		}else {
+			String sessionID = session.getId();
+			System.out.println("세션 ID : " + sessionID);
+		}
+		return mv;
+	}
+
+	@RequestMapping("/sessionNumberTest.do")
+	public ModelAndView sessionNumberTest(HttpSession session) {
+		ModelAndView mv = new ModelAndView("main");
+
+		Integer randomNumber = (Integer)session.getAttribute("random-number");
+		if(randomNumber  == null) {
+			Integer generated = new Random().nextInt();
+			System.out.println("generate-random-number : " + generated.toString());
+			session.setAttribute("random-number",generated);
+		}else {
+			System.out.println("my random umber : "+randomNumber.toString());
+		}
+
+		return mv;
+	}
+
+	@GetMapping("/CookieTest.do")
+	public ModelAndView mair(HttpServletResponse response) throws UnsupportedEncodingException {
+		//Cookie cookie = new Cookie("message","from_main");
+		ModelAndView mv = new ModelAndView("main");
+				
+		Cookie cookie = new Cookie("message", URLEncoder.encode("from main", "UTF-8"));
+		response.addCookie(cookie);
+		return mv;
+	}
+
+	@RequestMapping("/getCookie.do")
+	public ModelAndView getCookie(@CookieValue(value="message", required=false) Cookie cookie) throws UnsupportedEncodingException {
+		ModelAndView mv = new ModelAndView("main");
+
+		if(cookie !=null) {
+			String value = cookie.getValue();
+			System.out.println("cookie message : " + URLDecoder.decode(value, "UTF-8"));
+		}
+
 		return mv;
 
 	}
